b5f3ba54135373792580d99f42e448a4fda9cc12
arvinr16 <arviansyahyk@gmail.com>
2025-07-19 12:45:50 +0700

    Adding 'media query min-width: 320px' (notFinishYet0)

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ document.addEventListener('click', (e) => {
         navLink.classList.remove('open');
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const mediaQuery = window.matchMedia("(min-width: 372px)");
+    const imgContainer = document.querySelector(".div-landing-img");
+    const image = imgContainer?.querySelector("img");
+    const target = document.querySelector(".btn-landing");
+
+    const originalParent = image?.parentNode;
+    const originalNextSibling = image?.nextSibling;
+
+    function handleResize(e) {
+        if(!image || !target || !originalParent) return;
+
+        if(e.matches) {
+            target.parentNode.insertBefore(image, target);
+        } else {
+            if(originalNextSibling) {
+                originalParent.insertBefore(image, originalNextSibling);
+            } else {
+                originalParent.appendChild(image);
+            }
+        }
+    }
+
+    handleResize(mediaQuery);
+
+    mediaQuery.addEventListener("change", handleResize);
+});
